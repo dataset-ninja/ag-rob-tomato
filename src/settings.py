7adb08c0682cea174972d64f9a06a81a64d527ee
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-31 20:18:30 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,7 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = "AgRobTomato"
+PROJECT_NAME: str = "AgRobTomato Dataset"
 PROJECT_NAME_FULL: str = "AgRobTomato Dataset: Greenhouse Tomatoes With Different Ripeness Stages"
 HIDE_DATASET = False  # set False when 100% sure about repo quality
 
